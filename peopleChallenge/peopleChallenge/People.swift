ea901ebec7d758f9c80acd259cbd831cb31f0a06
Sara <saraahmad1114@yahoo.com>
2017-03-23 02:38:15 -0400

    added the People class

---
+++ peopleChallenge/peopleChallenge/People.swift
@@ -0,0 +1,23 @@
+//
+//  People.swift
+//  peopleChallenge
+//
+//  Created by Flatiron School on 3/23/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class People{
+
+    var name: String?
+    var favoriteCity: String?
+    var id: Int?
+    
+    init(name: String, favoriteCity: String, id: Int){
+        self.name = name
+        self.favoriteCity = favoriteCity
+        self.id = id
+    }
+
+}
