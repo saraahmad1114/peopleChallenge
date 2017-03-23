7e986ef33353bf6b6c669bd4365d9c97e0f64929
Sara <saraahmad1114@yahoo.com>
2017-03-23 03:03:28 -0400

    PeopleDataStore added

---
+++ peopleChallenge/peopleChallenge/PeopleDataStore.swift
@@ -0,0 +1,57 @@
+//
+//  PeopleDataStore.swift
+//  peopleChallenge
+//
+//  Created by Flatiron School on 3/23/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class PeopleDataStore{
+
+    static let sharedInstance = PeopleDataStore()
+    private init(){}
+    
+    var peopleArray : [People] = []
+    
+    func getPeopleInformation(completion:@escaping([People]) ->()){
+    
+        PeopleAPIClient.getPeopleInformation { (peopleArray) in
+            
+            if peopleArray.count == 0{
+                print("You do not have people")
+            }
+            else{
+            for singleDictionary in peopleArray{
+            
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else{print("unwrappedSingleDicitonary did not unwrap"); return}
+                
+                guard let unwrappedPersonName = unwrappedSingleDictionary["name"] as? String else{print("unwrappedPersonName did not unwrap"); return}
+                
+                guard let unwrappedPersonFavoriteCity = unwrappedSingleDictionary["favoriteCity"] as? String else{print("unwrappedPersonFavoriteCity did not unwrap"); return}
+                
+                guard let unwrappedPersonId = unwrappedSingleDictionary["id"] as? Int else{print("unwrappedPersonId did not unwrap"); return}
+                
+                let singlePersonObject = People.init(name: unwrappedPersonName, favoriteCity: unwrappedPersonFavoriteCity, id: unwrappedPersonId)
+                
+                self.peopleArray.append(singlePersonObject)
+                
+                print("****************PERSON INFORMATION**************")
+                print("PERSON NAME: \(singlePersonObject.name)")
+                print("PERSON FAVORITE CITY: \(singlePersonObject.favoriteCity)")
+                print("PERSON ID: \(singlePersonObject.id)")
+                print("************************************************")
+            }
+            completion(self.peopleArray)
+            }
+        }
+    
+    }
+
+
+
+
+
+
+}
