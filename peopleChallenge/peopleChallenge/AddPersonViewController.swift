e94350e7671435b78b2cb727d28c5e4d2e2cdb5a
Sara <saraahmad1114@yahoo.com>
2017-03-24 14:17:49 -0400

    added another view controller

---
+++ peopleChallenge/peopleChallenge/AddPersonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddPersonViewController.swift
+//  peopleChallenge
+//
+//  Created by Flatiron School on 3/24/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class AddPersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
