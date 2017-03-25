b97243ae7afcfe92e7c762da4e382d83a7038aa0
Sara <saraahmad1114@yahoo.com>
2017-03-25 04:13:40 -0400

    clean up

--- peopleChallenge/peopleChallenge/AddPersonViewController.swift
+++ peopleChallenge/peopleChallenge/AddPersonViewController.swift
@@ -28,31 +28,34 @@ class AddPersonViewController: UIViewController {
     @IBAction func addPersonButtonTapped(_ sender: Any) {
         
         let firstCondition = self.nameTextfield.text?.isEmpty
+        
         let secondCondition = self.favoriteCityTextfield.text?.isEmpty
         
         if firstCondition! && secondCondition! {
+            
             let alert = UIAlertController(title: "FORM MISSING INFORMATION", message: "You have not filled out the name and the favorite city", preferredStyle: UIAlertControllerStyle.alert)
+            
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
             self.present(alert, animated: true, completion: nil)
         }
         else {
             guard let personName = self.nameTextfield.text else{
-                print("personName did not unwrap"); return
-            }
+                print("personName did not unwrap"); return}
+            
             guard let personFavoriteCity = self.favoriteCityTextfield.text else{
-                print("personFavoriteCity did not unwrap"); return
-            }
+                print("personFavoriteCity did not unwrap"); return}
+            
             PeopleAPIClient.postPeopleInformation(nameVal: personName, favoriteCityVal: personFavoriteCity)
            
         }
     }
     
-    
-    
-    
+
     @IBAction func doneButtonTapped(_ sender: Any) {
+        
         self.dismiss(animated: false, completion: nil)
-        print("done")
+        
     }
 
 
--- peopleChallenge/peopleChallenge/PeopleDataStore.swift
+++ peopleChallenge/peopleChallenge/PeopleDataStore.swift
@@ -35,7 +35,6 @@ class PeopleDataStore{
 
             }
             completion(self.peopleArray)
-         
         }
     
     }
