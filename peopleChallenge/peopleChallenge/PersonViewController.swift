e8766fa6516ba2fb547dc0a8ca72d9f3a48c6623
Sara <saraahmad1114@yahoo.com>
2017-03-25 04:20:01 -0400

    minor changes

--- peopleChallenge/peopleChallenge/PersonViewController.swift
+++ peopleChallenge/peopleChallenge/PersonViewController.swift
@@ -26,25 +26,37 @@ class PersonViewController: UIViewController {
         guard let unwrappedPersonObject = personObject else{print("unwrappedPersonObject did not unwrap"); return}
         
         self.updateNameLabel.text = unwrappedPersonObject.name
+        
         self.updateCityLabel.text = unwrappedPersonObject.favoriteCity
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
     }
     
     
     @IBAction func changeCityButtonTapped(_ sender: Any) {
+        
         guard let unwrappedPersonObject = personObject else{print("unwrappedPersonObject did not unwrap"); return }
+        
         guard let neededId = unwrappedPersonObject.id else {print("neededId did not unwrap"); return }
+        
         guard let neededCity = self.updateCityTextfield.text else{print("neededCity did not unwrap"); return }
     
         if (updateCityTextfield.text?.isEmpty)!{
             print("You didn't change the city!")
         }
+            
         else {
+            
             PeopleAPIClient.putPeopleInformation(cityNameVal: neededCity, id: neededId)
             
+            let alert = UIAlertController(title: "UPDATE COMPLETE", message: "You have updated favorite city", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
         }
     }
  
